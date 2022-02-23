a9fd72a5f7ab93bfc1eb3803a5afc25ae9436bff
Samuel Leach <45556730+SamALeach@users.noreply.github.com>
2022-02-23 18:03:38 -0500

    edit template change

--- bin/activate.fish
+++ bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/samleach/Downloads/django-proj"
+set -gx VIRTUAL_ENV "/Users/samleach/Downloads/Django-project"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(django-proj) "
-            printf "%s%s" "(django-proj) " (set_color normal)
+        if test -n "(Django-project) "
+            printf "%s%s" "(Django-project) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
